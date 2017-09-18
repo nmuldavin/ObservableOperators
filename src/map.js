45fd9d828fc566c7e576c2be6402adbb2a561c9c
Noah Muldavin <nmuldavin@gmail.com>
2017-09-17 20:57:36 -0700

    Update map method documentation

--- src/map.js
+++ src/map.js
@@ -1,11 +1,11 @@
 import transform from './transform'
 
 /**
- * Returns a new Observable that emits the result of a provided operation applied to each
- * value received from an input observable
- * @param  {Observable} input [description]
- * @param  {Function}   fn    [description]
- * @return {Observable}         [description]
+ * Returns a new Observable that emits the result of a provided function applied to each
+ * value received from an input observable.
+ * @param  {Observable} input Input Observable
+ * @param  {Function}   fn    Mapping function
+ * @return {Observable}       New observable with mapped values
  */
 const map = (input, fn) =>
   transform(input, (observer, value) => observer.next(fn(value)))
