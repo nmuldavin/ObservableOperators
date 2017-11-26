18bee0fe83a11eaa7a3263cb26fec3b5f86d7418
Noah Muldavin <nmuldavin@gmail.com>
2017-11-25 16:46:54 -0800

    Make addCreators method to add static methods to an object

---
+++ src/addCreators.js
@@ -0,0 +1,24 @@
+/**
+ * Adds an array of methods to the specified target object. Does not modify
+ * the functions (unlike {@link addOperators}).
+ * @example
+ * // adding creators to Observable so you can call Observable.fromPromise(), etc
+ * addCreators(Observable, [fromEvent, fromPromise])
+ *
+ * @param {Object}          target   Target Object
+ * @param {Array<Function>} creators Array of functions
+ */
+
+const addCreators = (target, creators) =>
+  creators.forEach(creator => {
+    if (target[creator._name] === undefined) {
+      Object.defineProperty(target, creator._name, {
+        value: creator,
+        writable: true,
+        enumerable: false,
+        configurable: true
+      })
+    }
+  })
+
+export default addCreators
