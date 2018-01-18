ccd6cb8c71c09009a80ea441e79f6cc063feec61
Ryan Streur <ryan.j.streur@gmail.com>
2018-01-17 22:46:02 -0800

    add 'first' operator

--- src/index.js
+++ src/index.js
@@ -1,37 +1,92 @@
-import addOperators from './addOperators'
+import addOperators from './addOperators';
+import addCreators from './addCreators';
 
-import filter from './filter'
-import forEach from './forEach'
-import map from './map'
-import reduce from './reduce'
-import scan from './scan'
-import take from './take'
-import toArray from './toArray'
-import transform from './transform'
+import catchError from './catchError';
+import concat from './concat';
+import debounce from './debounce';
+import defer from './defer';
+import delay from './delay';
+import error from './error';
+import filter from './filter';
+import first from './first';
+import flatMap from './flatMap';
+import forEach from './forEach';
+import fromEvent from './fromEvent';
+import fromPromise from './fromPromise';
+import interval from './interval';
+import map from './map';
+import merge from './merge';
+import reduce from './reduce';
+import scan from './scan';
+import skip from './skip';
+import skipLast from './skipLast';
+import skipUntil from './skipUntil';
+import startWith from './startWith';
+import take from './take';
+import takeLast from './takeLast';
+import takeUntil from './takeUntil';
+import throttle from './throttle';
+import toArray from './toArray';
+import transform from './transform';
 
-const addAll = target => {
-  addOperators(target || Observable.prototype, [
+const addAll = (target = Observable) => {
+  addCreators(target, [defer, error, fromEvent, fromPromise, interval]);
+
+  addOperators(target.prototype, [
+    catchError,
+    concat,
+    debounce,
+    delay,
     filter,
+    first,
+    flatMap,
     forEach,
     map,
+    merge,
     reduce,
     scan,
+    skip,
+    skipLast,
+    skipUntil,
+    startWith,
     take,
+    takeLast,
+    takeUntil,
+    throttle,
     toArray,
-    transform
-  ])
-}
+    transform,
+  ]);
+};
 
 export {
   addAll as default,
   addAll,
   addOperators,
+  catchError,
+  concat,
+  debounce,
+  defer,
+  delay,
+  error,
   filter,
+  first,
+  flatMap,
   forEach,
+  fromEvent,
+  fromPromise,
+  interval,
   map,
+  merge,
   reduce,
   scan,
+  skip,
+  skipLast,
+  startWith,
   take,
+  takeLast,
+  takeUntil,
+  throttle,
+  skipUntil,
   toArray,
-  transform
-}
+  transform,
+};
